c4af2fa40d52d95e9b80c962d132404f68fa7b91
lhhee <lhhee@LAPTOP-PM4G7H13>
2021-12-02 09:32:55 +0900

    초기 커밋.

---
+++ AlgorithmTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AlgorithmTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  (  ')");
+            Console.WriteLine("(  /   )");
+            Console.WriteLine(" \\(__) |");
+        }
+    }
+}
