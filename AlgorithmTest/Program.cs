2236e7091849d511665e6d5c464a847917c33f33
poiu419 <poiu419@naver.com>
2021-12-02 15:36:38 +0900

    [4344] 평균은 넘겠지

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmTest
 {
@@ -6,20 +8,42 @@ namespace AlgorithmTest
     {
         static void Main(string[] args)
         {
-            string[] inputData = Console.ReadLine().Split(" ");
+            int inputCount = Convert.ToInt32(Console.ReadLine());
 
-            int a = Convert.ToInt32(inputData[0]);
-            int b = Convert.ToInt32(inputData[1]);
+            List<double> overAverage = new List<double>();
+            
+            for (int i = 0; i < inputCount; i++)
+            {
+                string[] inputData = Console.ReadLine().Split(" ");
+                List<int> scorelist = new List<int>();
 
-            if(a > b)
+                foreach (string item in inputData)
+                {   
+                    scorelist.Add(Convert.ToInt32(item));
+                }
+
+                double repeatCount = scorelist[0];
+
+                scorelist.RemoveAt(0);
+
+                double average = scorelist.Average();
+                double count = 0;
+
+                foreach (int score in scorelist)
+                {
+                    if(score > average)
+                    {
+                        count++;
+                    }
+                }
+
+                double percent = count / repeatCount * 100;
+                overAverage.Add(percent);
+            }
+
+            foreach (double percent in overAverage)
             {
-                Console.WriteLine(">");
-            }else if(a < b)
-            {
-                Console.WriteLine("<");
-            }else if(a == b)
-            {
-                Console.WriteLine("==");
+                Console.WriteLine(String.Format("{0:0.000}", percent) + "%");
             }
         }
     }
