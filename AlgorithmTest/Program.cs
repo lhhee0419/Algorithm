f999810b07fc6e1335dc9a45f20765a011adcf39
poiu419 <poiu419@naver.com>
2021-12-06 17:49:54 +0900

    [2884] 알람 시계

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -6,24 +6,28 @@ namespace AlgorithmTest
     {
         static void Main(string[] args)
         {
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
+            string[] inputData = Console.ReadLine().Split(" ");
 
-            if(x >= 0 && y >= 0)
+            int h = Convert.ToInt32(inputData[0]);
+            int m = Convert.ToInt32(inputData[1]);
+
+            if( m >= 45)
             {
-                Console.WriteLine("1");
-            }else if(x >= 0 && y<=0)
+                m = m - 45;
+            }else
             {
-                Console.WriteLine("4");
+                if(h == 0)
+                {
+                    h = 23;
+                   
+                }else
+                {
+                    h = h - 1;
+                }
+                m = 60 - (45 - m);
             }
-            else if (x <= 0 && y >= 0)
-            {
-                Console.WriteLine("2");
-            }
-            else
-            {
-                Console.WriteLine("3");
-            }
+
+            Console.WriteLine(h + " " + m);
         }
     }
 }
