f36550ea9d1ef7edf90bdd47aa99d3a441bc7da6
poiu419 <poiu419@naver.com>
2021-12-07 10:30:12 +0900

    [2438] 별 찍기 - 1

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -8,9 +8,13 @@ namespace AlgorithmTest
         {
             int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 1; i < 10; i++)
+            for (int i = 1; i < n + 1; i++)
             {
-                Console.WriteLine($"{n} * {i} = {n * i}");
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
         }
     }
