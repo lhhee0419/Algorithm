f18255b953719bbceeb5166b7221717725003da6
poiu419 <poiu419@naver.com>
2021-12-08 09:01:27 +0900

    [10951] A+B - 4

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -6,33 +6,21 @@ namespace AlgorithmTest
     class Program
     {
         static void Main(string[] args)
-        {
-            int input = Convert.ToInt32(Console.ReadLine());
-            int newNum = input;
-
-            int n = 0, n1 = 0, n10 = 0, count = 0;
-
-            while (true)
+        {   
+            while(true)
             {
-                if (input == newNum && count != 0)
+                string input = Console.ReadLine();
+
+                if (input == null || input == "")
                     break;
 
-                if (newNum >= 10)
-                { 
-                    n10 = newNum / 10;
-                }
-                else
-                {
-                    n10 = 0;
-                }
+                string[] inputData = input.Split(" ");
 
-                n1 = newNum % 10;
-                n = (n1 + n10) % 10;
+                int a = Convert.ToInt32(inputData[0]);
+                int b = Convert.ToInt32(inputData[1]);
 
-                newNum = Convert.ToInt32(n1.ToString() + n.ToString());
-                count++;
-            }
-            Console.WriteLine(count.ToString());
+                Console.WriteLine(a + b);
+            }    
         }
     }
 }
