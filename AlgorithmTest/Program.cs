24356e438ca03ba2981f24e1ffa3397e309fa927
poiu419 <poiu419@naver.com>
2021-12-08 16:39:29 +0900

    [2869] 달팽이는 올라가고싶다

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -8,9 +8,21 @@ namespace AlgorithmTest
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
+            string[] inputData = Console.ReadLine().Split(" ");
 
-            Console.WriteLine(Convert.ToInt32(Convert.ToChar(s)));
+            int a = Convert.ToInt32(inputData[0]);
+            int b = Convert.ToInt32(inputData[1]);
+            int v = Convert.ToInt32(inputData[2]);
+
+            int result = (v - b) / (a - b);
+            int r = (v - b) % (a - b);
+
+            if (r > 0)
+            {
+                result += 1;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
