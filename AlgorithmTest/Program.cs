4762fdfb968ac50bb2725b362938112f0c925787
poiu419 <poiu419@naver.com>
2021-12-08 10:08:08 +0900

    [10818] 최소, 최대

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmTest
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            while(true)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] inputData = Console.ReadLine().Split(" ");
+
+            List<int> numList = new List<int>();
+
+            foreach (string item in inputData)
             {
-                string input = Console.ReadLine();
-
-                if (input == null || input == "")
-                    break;
-
-                string[] inputData = input.Split(" ");
-
-                int a = Convert.ToInt32(inputData[0]);
-                int b = Convert.ToInt32(inputData[1]);
-
-                Console.WriteLine(a + b);
-            }    
+                numList.Add(Convert.ToInt32(item));
+            }
+            Console.Write(numList.Min() + " " + numList.Max());
         }
     }
 }
