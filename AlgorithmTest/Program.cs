41ef688602b1b31a9886ebc70bdfdc982e9608fc
poiu419 <poiu419@naver.com>
2021-12-27 15:42:25 +0900

    [2577] 숫자의 개수

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace AlgorithmTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string[] inputData = Console.ReadLine().Split(" ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
 
-            int a = Convert.ToInt32(inputData[0]);
-            int b = Convert.ToInt32(inputData[1]);
-            int v = Convert.ToInt32(inputData[2]);
+            int sum = a * b * c;
 
-            int result = (v - b) / (a - b);
-            int r = (v - b) % (a - b);
+            string strSum = sum.ToString();
 
-            if (r > 0)
+            int[] counts = new int[10];
+
+
+            foreach (char item in strSum)
             {
-                result += 1;
+                counts[Convert.ToInt32(item)-48]++;
             }
 
-            Console.WriteLine(result);
+            foreach (int count in counts)
+            {
+                Console.WriteLine(count);
+            }
         }
     }
 }
