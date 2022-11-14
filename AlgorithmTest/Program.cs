656cf352d9da81f660b633d9dadd3a910833deca
poiu419 <poiu419@naver.com>
2022-11-14 13:22:45 +0900

    [완전탐색]최소직사각형

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -7,22 +7,33 @@ namespace AlgorithmTest
     {
         static void Main(string[] args)
         {
-            int k = 4;  //사과의 최고점
-            int m = 3;  //사과 포장 갯수
-            int[] score = new int[] { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 }; //사과점수
-            
+            //{{60, 50}, {30, 70}, {60, 30}, {80, 40}}	4000
+            //{{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}	120
+            //{{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}	133
+            int[,] sizes = new int[,] { { 60, 50 }, { 30, 70 }, {60, 30}, {80, 40} };
+
             int answer = 0;
 
-            Array.Sort(score);
-            Array.Reverse(score);
+            int max1 = 0;
+            int max2 = 0;
 
-            int length = score.Length;
-            int boxQty = length / m;
-
-            for (int i = 1; i <= boxQty; i++)
+            for (int i = 0; i < sizes.Length / 2; i++)
             {
-                answer += score[(i * m) - 1] * m;
+                if(sizes[i,0].CompareTo(sizes[i, 1]) == -1) //왼쪽값 보다 오른쪽 값이 클 경우 위치 변경
+                {
+                    int x = sizes[i, 0];
+                    sizes[i, 0] = sizes[i, 1]; 
+                    sizes[i, 1] = x; 
+                }
+
+                if (max1.CompareTo(sizes[i, 0]) == -1)
+                    max1 = sizes[i, 0];
+
+                if (max2.CompareTo(sizes[i, 1]) == -1)
+                    max2 = sizes[i, 1];
             }
+
+            answer = max1 * max2;
 
             Console.WriteLine(answer);
         }
