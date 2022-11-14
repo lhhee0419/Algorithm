f293b6309c513031b3a3c8d4253a54e50a05c395
poiu419 <poiu419@naver.com>
2022-11-14 11:27:24 +0900

    Lv1.과일장수

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgorithmTest
 {
@@ -6,9 +7,24 @@ namespace AlgorithmTest
     {
         static void Main(string[] args)
         {
-            int year = int.Parse(Console.ReadLine());
+            int k = 4;  //사과의 최고점
+            int m = 3;  //사과 포장 갯수
+            int[] score = new int[] { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 }; //사과점수
+            
+            int answer = 0;
 
-            Console.WriteLine(year - 543);
+            Array.Sort(score);
+            Array.Reverse(score);
+
+            int length = score.Length;
+            int boxQty = length / m;
+
+            for (int i = 1; i <= boxQty; i++)
+            {
+                answer += score[(i * m) - 1] * m;
+            }
+
+            Console.WriteLine(answer);
         }
     }
 }
